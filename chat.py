17e836ac98f12d04edb5ad3b0e7036caf0e4fb03
Nandakumar005 <nandakumardg8@gmail.com>
2026-04-19 18:27:46 +0530

    test

--- chat.py
+++ chat.py
@@ -3,14 +3,14 @@ chat_history=[]
 
 while True:
     user_input=input("you: ")
-    if user_input.lower in ['exit','end','bye']:
+    if user_input.lower() in ['exit','end','bye']:
         break
     else:
         chat_history.append({'role':'user','content':user_input})
         response=ollama.chat(
         model='gemma3:1b',messages=chat_history)
 
-        bot_replay=response['message']['content']
+        bot_replay=response['message']['content'],stream=True
         print("bot:",bot_replay)
 
         chat_history.append({'role':'assistant','content':bot_replay})
