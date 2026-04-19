c4c7544f1d4c22bb08805a7e1c96b6e43b9ed90f
Nandakumar005 <nandakumardg8@gmail.com>
2026-04-19 21:34:57 +0530

    added memory and token counting

--- testing.py
+++ testing.py
@@ -1,20 +1,41 @@
 from ollama import chat
+import tiktoken
 
-instruction=input("enter the system instruction:... ")
+enc = tiktoken.get_encoding("cl100k_base")
+instruction = "you are a friendly ai assistant"
+# token counter
+def count_tokens(messages):
+    total = 0
+    for m in messages:
+        total += len(enc.encode(m["content"]))
+    return total
+
+# store conversation history
+messages = [
+    {"role": "system", "content": instruction}
+]
 while True:
     user_input = input("you: ")
     if user_input.lower() in ['exit', 'end', 'bye']:
         break
-
+    # add user message
+    messages.append({"role": "user", "content": user_input})
     stream = chat(
         model='gemma3:1b',
-        messages=[
-        {'role': 'system', 'content': instruction},
-        {'role': 'user', 'content': user_input}
-        ],
+        messages=messages,
         stream=True,
     )
-    for chunk in stream:
-        print(chunk['message']['content'], end='', flush=True)
+    full_reply = ""
 
-    print()  
+    for chunk in stream:
+        content = chunk['message']['content']
+        full_reply += content
+        print(content, end='', flush=True)
+
+    print()
+
+    # add assistant reply to memory
+    messages.append({"role": "assistant", "content": full_reply})
+
+    # print token usage
+    print("Tokens:", count_tokens(messages))
